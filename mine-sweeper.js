51f553a3d5e505552874f507ba5485b7f9fbeb75
kkoonn <worldishungry_paper@yahoo.co.jp>
2020-03-07 17:07:39 +0900

    簡易的なHTML出力実装

--- mine-sweeper.js
+++ mine-sweeper.js
@@ -1,12 +1,42 @@
 // まいんすいーぱー
 const startButton = document.getElementById('start');
+const gameDivided = document.getElementById('game-area');
 
 const mineNum = 100;
 const height = 16;
 const width = 30;
 const cellNum = height * width
-var field = new Array(cellNum);
+var field = new Array()
 
+/**
+ * 指定した要素の子どもをすべて削除する
+ * @param {HTMLElement} element HTMLの要素
+ */
+function removeAllChildren(element) {
+    while (element.firstChild) {  // 子どもの要素がある限り削除
+        element.removeChild(element.firstChild);
+    }
+}
+
+// fieldの初期化
+function initField() {
+    // game-areaに描画されているfieldを消去
+    removeAllChildren(gameDivided);
+
+    // 新たなfield配列を生成
+    let retField = new Array(cellNum);
+    for (let i = 0; i < cellNum; i++) {
+        let cell = {
+            isMine: false,      // 地雷が設置されているかどうか
+            aroundMineNum: 0,   // 周囲の地雷の数
+            isOpened: false,    // プレイヤーによって開けられているかどうか
+            button: null        // HTML出力用変数
+        }
+        cell.button = document.createElement('button');
+        retField[i] = cell
+    }
+    return retField;
+}
 // 地雷の生成
 function makeMineMap() {
     // 連番リスト・地雷の連想配列を生成
@@ -48,8 +78,8 @@ function countAroundMineNum(I, J) {
     }
     return count;
 }
-// Consoleにfieldを出力する
-function displayFieldAtConsole(){
+// Consoleでfieldを出力する
+function displayFieldWithConsole() {
     for (let i = 0; i < height; i++) {
         let string = "";
         for (let j = 0; j < width; j++) {
@@ -63,22 +93,29 @@ function displayFieldAtConsole(){
         console.log(string);
     }
 }
+// game-areaにfieldを出力する
+function displayFieldWithHTML() {
+    for(let i=0;i<height;i++){
+        for(let j=0;j<width;j++){
+            pos = i * width + j;
+            gameDivided.appendChild(field[pos].button);
+        }
+        let br = document.createElement('br');
+        gameDivided.appendChild(br);
+    }
+}
 
 // ゲームの初期化
 startButton.onclick = () => {
+    // field初期化
+    field = initField();
     // 地雷の生成
     const mineMap = makeMineMap();
     console.log('mineNum: ' + mineNum + ' mineMapLength: ' + Object.keys(mineMap));
     console.log(mineMap);
     // 地雷をfieldに設置
     for (let i = 0; i < cellNum; i++) {
-        let cell = {
-            isMine: false,      // 地雷が設置されているかどうか
-            aroundMineNum: 0,   // 周囲の地雷の数
-            isOpened: false     // プレイヤーによって開けられているかどうか
-        }
-        cell.isMine = mineMap.get(i);
-        field[i] = cell
+        field[i].isMine = mineMap.get(i);
     }
     console.log('field(周囲の地雷カウント前)');
     console.log(field);
@@ -86,10 +123,12 @@ startButton.onclick = () => {
     for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++) {
             field[i * width + j].aroundMineNum = countAroundMineNum(i, j);
+            field[i * width + j].button.innerText = field[i * width + j].aroundMineNum
         }
     }
     console.log('field(周囲の地雷カウント後)');
     console.log(field);
     // TODO fieldの描画
-    displayFieldAtConsole();
+    displayFieldWithConsole();
+    displayFieldWithHTML();
 }
